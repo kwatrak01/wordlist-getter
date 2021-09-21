7dcf80764dac905b58ca1509321f215913c0d72a
Konrad Watrak <81235938+kwatrak01@users.noreply.github.com>
2021-09-21 15:03:05 +0200

    Initial

---
+++ finder.php
@@ -0,0 +1,33 @@
+<form action="finder.php" method="get">
+    <input type="text" name="letter" id="letter" placeholder="Na jaką literę chcesz słówka?" value="<?= isset($_GET['letter']) ? $_GET['letter'] : '' ?>">
+    <input type="number" name="page" id="page" placeholder="Z której strony chcesz słówka?" min="1" value="<?= isset($_GET['page']) ? $_GET['page'] : '' ?>">
+    <button type="submit">Szukaj</button>
+</form>
+<?php
+
+require_once('simple_html_dom.php');
+
+if (isset($_GET['letter']) && isset($_GET['page'])) {
+    $html = file_get_html("https://ling.pl/slownik/polsko-angielski/slowa/{$_GET['letter']}/{$_GET['page']}");
+
+    $result = $html->find("div[class=col-md-4] > a[href*=/slownik/polsko-angielski/]");
+
+    $i = 0;
+    foreach ($result as $r) {
+
+        echo $r->plaintext . "<br>";
+
+        $i++;
+    }
+
+    $pagination = $html->find("ul[class=pagination] > li");
+    echo "<ul>";
+    foreach ($pagination as $p) {
+        $next = $p->first_child()->plaintext;
+
+        $p->first_child()->href = "/pai/finder.php?letter={$_GET['letter']}&page={$next}";
+        echo $p;
+    }
+    echo "</ul>";
+}
+?>
